3ae3e255c406e1eec6a03afe267d081d8906ce5a
Christopher Teddy Mienarto <46671469+christoted@users.noreply.github.com>
2021-04-09 17:14:38 +0700

    feat/ui-login

--- app/src/main/java/com/example/tedgram/presentation/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/tedgram/presentation/ui/login/LoginActivity.kt
@@ -1,12 +1,31 @@
 package com.example.tedgram.presentation.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tedgram.R
+import com.example.tedgram.databinding.ActivityLoginBinding
+import com.example.tedgram.presentation.ui.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private var _binding: ActivityLoginBinding ?= null
+    private val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+        binding?.tvRegister?.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
