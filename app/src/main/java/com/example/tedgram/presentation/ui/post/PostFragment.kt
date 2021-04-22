0313a43dfc35361cec1d25fe2e3ad4b339a6c5b8
Christopher Teddy Mienarto <46671469+christoted@users.noreply.github.com>
2021-04-22 20:35:03 +0700

    feat/progresss

--- app/src/main/java/com/example/tedgram/presentation/ui/post/PostFragment.kt
+++ app/src/main/java/com/example/tedgram/presentation/ui/post/PostFragment.kt
@@ -1,33 +1,47 @@
 package com.example.tedgram.presentation.ui.post
 
+import android.Manifest
+import android.R.attr
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.tedgram.R
+import com.example.tedgram.databinding.FragmentPostBinding
+import com.theartofdev.edmodo.cropper.CropImage
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PostFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PostFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentPostBinding? = null
+    private val binding get() = _binding
+
+    private lateinit var cropActivityResultLauncher: ActivityResultLauncher<Any?>
+
+    private var imageUrii: Uri? = null
+
+    companion object {
+        private val URI_KEY = "uri"
+    }
+
+    private val cropActivityResultContract = object : ActivityResultContract<Any?, Uri?>() {
+        override fun createIntent(context: Context, input: Any?): Intent {
+            return CropImage.activity()
+                .getIntent(context)
         }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+            return CropImage.getActivityResult(intent)?.uri
+        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +49,54 @@ class PostFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false)
+        _binding = FragmentPostBinding.inflate(layoutInflater, container, false)
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PostFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity == null) {
+            return
+        }
+
+        imageUrii = savedInstanceState?.getParcelable(URI_KEY)
+
+        if ( imageUrii != null) {
+            Log.d("OY", "onViewCreated gak null: $imageUrii")
+        }
+
+        Log.d("OY", "onViewCreated: null $imageUrii")
+
+        val radius = resources.getDimension(R.dimen.corner_radius)
+        binding?.imageView?.shapeAppearanceModel?.toBuilder()?.setAllCornerSizes(radius)?.build()
+
+        cropActivityResultLauncher =
+            activity!!.registerForActivityResult(cropActivityResultContract) {
+                it.let {
+                    binding?.iconAdd?.visibility = View.GONE
+                    imageUrii = it
+                    binding?.imageView?.setImageURI(imageUrii)
+
                 }
+
             }
+
+        binding?.iconAdd?.setOnClickListener {
+            cropActivityResultLauncher.launch(null)
+
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("OY", "onSaveInstanceState: PALING BAWAH $imageUrii")
+        outState.putParcelable(URI_KEY, imageUrii)
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        _binding = null
     }
 }
